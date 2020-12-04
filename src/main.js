f8b3161ad9d39e06a70376ed30565b4b8c2dbd8e
Luísa Oliveira Sousa <luisasousa1@live.com>
2020-12-03 21:05:20 -0300

    🔨- Implemented map with method caller on click

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import panZoom from 'vue-panzoom'
+import '@/styles/index.scss'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(panZoom)
+Vue.use(Buefy)
 
 new Vue({
   render: h => h(App),
---
+++ src/views/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+     <div class="title">
+         <!-- <h1>Diagrama de influência<br>do Sistema de Obesidade</h1> -->
+         <!-- <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+          <img class="reference-button" src='info.svg'>
+        </a> -->
+     </div>
+    <div id="container" class="float-right">
+      <ObesityDiagram />
+    </div>
+    <!-- <div class="legend"> -->
+      <!-- <img src="legenda.svg" /> -->
+      <!-- <ObesityDiagram /> -->
+    <!-- </div> -->
+    <div class="collapse references" id="collapseExample">
+       <h2>Referência:</h2>
+       <a id="demo">Clique nos itens do mapa para mais detalhes</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import ObesityDiagram from "@/components/ObesityDiagram"
+
+export default {
+  name: 'Layout',
+  components: {
+    ObesityDiagram
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#container{
+  border: none!important
+}
+</style>
