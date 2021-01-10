02dad93b80ad2913f8c6bbd8f14244f1baede3e5
Luísa Oliveira Sousa <luoliveira96@gmail.com>
2021-01-10 18:14:15 -0300

    📱 - Mobile optimization

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div id="container">
     <div class="title">
          <h1>Diagrama de influência<br>do Sistema de Obesidade</h1>
         <font-awesome-icon id="information" icon="info-circle" />
     </div>
     <ObesityDiagram @update="handleUpdate" class="map" />
-    <div id="helpers">
+    <div id="helpers" v-if="isDesktop">
       <Reference class="reference"  :descriptionReference="descriptionReference"/>
       <Subtitle />
     </div>
@@ -26,12 +26,30 @@ export default {
   },
   data(){
     return{
-      descriptionReference: ''
+      descriptionReference: '',
+      screenSize: screen.width
+    }
+  },
+  computed: {
+    isDesktop() {
+      if (screen.width >= 1366) return true;
+      else  this.$buefy.dialog.alert({
+          message: 'Essa aplicação é melhor visualizada em modo retrato no celular. Para exibição detalhada, acesse pelo computador',
+          confirmText: 'OK'
+          });
+          return false;
     }
   },
   methods:{
     handleUpdate(description){
       this.descriptionReference = description
+    },
+    alertMobile() {
+      this.$buefy.dialog.alert({
+          title: 'Title Alert',
+          message: 'I have a title, a custom button and <b>HTML</b>!',
+          confirmText: 'Cool!'
+      })
     }
   }
 }
